b8fa3891c777e4519e125418c73162ab72cf0637
meganthadev <meganthadev@gmail.com>
2020-08-10 01:09:43 -0700

    Update category contr

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,20 @@
 class CategoriesController < ApplicationController
+
+    def index 
+       categories = Category.all
+       render json: CategorySerializer.new(categories).to_serialized_json
+    end 
+
+    def show 
+        category = Category.find(params[:id])
+        render json: CategorySerializer.new(category).to_serialized_json
+    end 
+
+    private
+
+    def category_params(*args)
+        params.require(:category).permit(*args)
+    end
+
+
 end
