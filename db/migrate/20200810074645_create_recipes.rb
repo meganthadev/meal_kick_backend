c514103d04d8681220f0969b102849a208e958c1
meganthadev <meganthadev@gmail.com>
2020-08-10 01:02:26 -0700

    Update recipe table

--- db/migrate/20200810074645_create_recipes.rb
+++ db/migrate/20200810074645_create_recipes.rb
@@ -4,6 +4,7 @@ class CreateRecipes < ActiveRecord::Migration[6.0]
       t.string :title
       t.string :ingredients
       t.text :instructions
+      t.integer :category_id, null: false
 
       t.timestamps
     end
