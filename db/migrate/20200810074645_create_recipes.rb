970fe580475e7c7a4da3056730c543a1d330879d
meganthadev <meganthadev@gmail.com>
2020-08-10 00:54:41 -0700

    Add recipe db

---
+++ db/migrate/20200810074645_create_recipes.rb
@@ -0,0 +1,11 @@
+class CreateRecipes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.string :ingredients
+      t.text :instructions
+
+      t.timestamps
+    end
+  end
+end
