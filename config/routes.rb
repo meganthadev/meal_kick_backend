5142f206612d61b1a3c13f3060c9142db8d8d65c
meganthadev <meganthadev@gmail.com>
2020-08-10 00:56:41 -0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  #resources :users
-  get 'users/test', to: 'users#test'
+  resources :categories
+  resources :recipes
+  root "application#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
