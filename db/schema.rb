ed67ef1847b1060163a8ef0293002d5d91d12cd2
meganthadev <meganthadev@gmail.com>
2020-08-11 15:54:50 -0700

    Remove users table

--- db/schema.rb
+++ db/schema.rb
@@ -31,13 +31,5 @@ ActiveRecord::Schema.define(version: 2020_08_11_210457) do
     t.index ["category_id"], name: "index_recipes_on_category_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   add_foreign_key "recipes", "categories"
 end
