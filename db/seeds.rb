a75e1cbf5920aea1d92b928a48f22b964dee4548
meganthadev <meganthadev@gmail.com>
2020-08-10 22:48:55 -0700

    Add seed cats

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+breakfast = Category.create(name: "Breakfast")
+lunch = Category.create(name: "Lunch")
+snacks = Category.create(name: "Snacks")
+dinner = Category.create(name: "Dinner")
+dessert = Category.create(name: "Desssert")
+drinks = Category.create(name: "Drinks")
