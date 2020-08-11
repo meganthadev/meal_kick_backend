995f76cd3e49253cf33be2538c38c0849966dac8
meganthadev <meganthadev@gmail.com>
2020-08-10 22:48:38 -0700

    Remove unnecc code

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,20 +1,6 @@
 class CategoriesController < ApplicationController
 
-    def index 
-       categories = Category.all
-       render json: CategorySerializer.new(categories).to_serialized_json
-    end 
-
-    def show 
-        category = Category.find(params[:id])
-        render json: CategorySerializer.new(category).to_serialized_json
-    end 
-
-    private
-
-    def category_params(*args)
-        params.require(:category).permit(*args)
-    end
+  
 
 
 end
