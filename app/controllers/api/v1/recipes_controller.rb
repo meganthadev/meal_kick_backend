e63e247da0d9a2eecaa4da91f86911d67a0d46c6
meganthadev <meganthadev@gmail.com>
2020-08-11 17:22:45 -0700

    Add index and create actions

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,5 +1,23 @@
 class Api::V1::RecipesController < ApplicationController
 
+    def index 
+        recipes = Recipe.all
+        render json: recipes
+    end 
 
-    
+    def create 
+        recipe = Recipe.new(recipe_params)
+        if recipe.save 
+            render json: recipe, status: :accepted
+        else  
+            render json: {errors: recipe.errors.full_messages}, status: :unproccesible_entity
+        end 
+    end 
+
+    private
+
+    def recipe_params
+        params.require(recipe).permit(:title, :ingredients, :instructions, :category_id)
+    end 
+
 end
