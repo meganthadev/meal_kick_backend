ea5af70df24f65a58b32c7d391373ea16770c5a6
meganthadev <meganthadev@gmail.com>
2020-08-11 19:01:27 -0700

    Add null:false attr

--- db/migrate/20200811210357_add_category_ref_to_recipes.rb
+++ db/migrate/20200811210357_add_category_ref_to_recipes.rb
@@ -1,5 +1,5 @@
 class AddCategoryRefToRecipes < ActiveRecord::Migration[6.0]
   def change
-    add_reference :recipes, :category, foreign_key: true
+    add_reference :recipes, :category, null: false, foreign_key: true
   end
 end
