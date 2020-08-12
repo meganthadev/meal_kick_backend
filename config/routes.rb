95aa53d6cf12cdffef99069300744b2bffdca311
meganthadev <meganthadev@gmail.com>
2020-08-11 18:46:39 -0700

    Add category api routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
       resources :recipes, only: [:index, :create]
     end 
   end 
+    namespace :api do
+      namespace :v1 do
+        resources :categories, only: [:index, :show]
+      end 
+    end
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
