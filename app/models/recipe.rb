be0213608e10aa67cc52d4284586f4d54ce3b164
meganthadev <meganthadev@gmail.com>
2020-08-11 19:30:50 -0700

    Add validations to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,7 @@
 class Recipe < ApplicationRecord
     belongs_to :category
+
+    validates :title, presence: true
+    validates :ingredients, presence: true
+    validates :instructions, presence: true
 end
